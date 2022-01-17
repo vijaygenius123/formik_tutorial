e31faa07542d9c79f7d9365cafd8152860e8e026
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2022-01-17 17:35:59 +0000

    Basic Setup

---
+++ src/components/YoutubeForm.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const YoutubeForm = () => {
+    return (
+        <>
+            <div>
+                <form>
+                    <label htmlFor="name">Name</label>
+                    <input type="text" id="name" name="name"/>
+
+                    <label htmlFor="email">Email Address</label>
+                    <input type="text" id="email" name="email"/>
+
+                    <label htmlFor="channel">Channel</label>
+                    <input type="text" id="channel" name="channel"/>
+
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        </>
+    )
+}
+
+export default YoutubeForm
