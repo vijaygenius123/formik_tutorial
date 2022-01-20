bc03cce6901e5f006925398137c7f5abf86c2fb4
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2022-01-20 08:49:07 +0000

    Replace name, onChange, onBlur with getFieldProps

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -28,22 +28,20 @@ const YoutubeForm = () => {
 
                     <div className='form-control'>
                         <label htmlFor="name">Name</label>
-                        <input type="text" id="name" name="name" onChange={formik.handleChange}
-                               value={formik.values.name}/>
+                        <input type="text" id="name" {...formik.getFieldProps('name')}
+                        />
                         {formik.errors.name && <div className='error-text'>{formik.errors.name}</div>}
                     </div>
 
                     <div className='form-control'>
                         <label htmlFor="email">Email Address</label>
-                        <input type="text" id="email" name="email" onChange={formik.handleChange}
-                               value={formik.values.email}/>
+                        <input type="text" id="email"  {...formik.getFieldProps('email')}/>
                         {formik.errors.email && <div className='error-text'>{formik.errors.email}</div>}
                     </div>
 
                     <div className='form-control'>
                         <label htmlFor="channel">Channel</label>
-                        <input type="text" id="channel" name="channel" onChange={formik.handleChange}
-                               value={formik.values.channel}/>
+                        <input type="text" id="channel" {...formik.getFieldProps('channel')}/>
                         {formik.errors.channel && <div className='error-text'>{formik.errors.channel}</div>}
                     </div>
                     <button className='btn btn-primary w-100' type="submit">Submit</button>
